697191897d4fe1bfb008b40ed6bb2851df6cd58e
subinyun <subinyun1124@gmail.com>
2025-06-18 17:06:18 +0900

    Time: 0 ms (100%), Space: 40.7 MB (18.12%) - LeetHub

---
+++ 0070-climbing-stairs/0070-climbing-stairs.java
@@ -0,0 +1,19 @@
+class Solution {
+    public int climbStairs(int n) {
+        if(n==1) return 1;
+        if(n==2) return 2;
+
+        // 피보나치 1,1,2,3,5,8,13
+        int first = 1;
+        int second = 2;
+
+        for (int i = 3; i <= n; i++) {
+            int third = first + second;
+            first = second;
+            second = third;
+        }
+
+        return second;
+
+    }
+}
